915fbbdd10488be06b7f52b05ad7edb79c60f83c
dotis90 <dotis90@gmail.com>
2020-05-28 13:11:55 -0400

    Add basic navbar and landing page

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Landing = () => {
+  return (
+    <div>
+      <h1>Welcome to Cinema Tracker. Find and track your favorite movies.</h1>
+    </div>
+  );
+};
+
+export default Landing;
