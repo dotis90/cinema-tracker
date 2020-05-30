aaa31e54d5fb8cdea72ed9d199167daf267f9e56
dotis90 <dotis90@gmail.com>
2020-05-30 11:54:50 -0400

    redux added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import Navibar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
@@ -9,18 +11,20 @@ import MovieDetailPage from './components/MovieDetailPage/MovieDetailPage';
 
 const App = () => {
   return (
-    <Router>
-      <Fragment>
-        <Navibar />
-        <Route exact path="/" component={Landing} />
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/main" component={MainPage} />
-          <Route exact path="/movies/:movieId" component={MovieDetailPage} />
-        </Switch>
-      </Fragment>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navibar />
+          <Route exact path="/" component={Landing} />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/main" component={MainPage} />
+            <Route exact path="/movies/:movieId" component={MovieDetailPage} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
