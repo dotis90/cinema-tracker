12677badcf3c69525a99ede9629136435857470d
dotis90 <dotis90@gmail.com>
2020-06-12 16:43:28 -0400

    small changes for production

---
+++ client/build/precache-manifest.608b04146665604c7a1b0fce8bf01da9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e736a2da6785225b531843ed3d488a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7cf69ccfd7ff25d8e8d",
+    "url": "/static/css/main.fc3b755d.chunk.css"
+  },
+  {
+    "revision": "e59507f6022abf73562f",
+    "url": "/static/js/2.1465b84e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1465b84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cf69ccfd7ff25d8e8d",
+    "url": "/static/js/main.29a962b5.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
